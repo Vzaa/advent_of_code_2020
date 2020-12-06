789374253949e6307b46c52ec3c2757c57cef3f3
Vzaa <Vzaa@users.noreply.github.com>
2020-12-06 10:16:05 +0300

    Day 05 Better

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,56 +1,43 @@
-use std::collections::HashMap;
-
-fn id(r: i32, c: i32) -> i32 {
-    r * 8 + c
-}
+use std::collections::HashSet;
 
 fn p1() {
     let instr = std::fs::read_to_string("input").unwrap();
 
-    let seats: HashMap<_, _> = instr
+    let max = instr
         .lines()
         .map(|line| {
-            let row_str = line[0..7].replace("F", "0").replace("B", "1");
-            let col_str = line[7..].replace("L", "0").replace("R", "1");
-
-            let row = i32::from_str_radix(&row_str, 2).unwrap();
-            let col = i32::from_str_radix(&col_str, 2).unwrap();
-
-            (id(row, col), (row, col))
+            let id = line
+                .chars()
+                .fold(0, |acc, c| acc << 1 | ((c == 'B' || c == 'R') as i32));
+            id
         })
-        .collect();
+        .max()
+        .unwrap();
 
-    println!("Part 1: {}", seats.keys().max().unwrap());
+    println!("Part 1: {}", max);
 }
 
 fn p2() {
     let instr = std::fs::read_to_string("input").unwrap();
 
-    let seats: HashMap<_, _> = instr
+    let seats: HashSet<_> = instr
         .lines()
         .map(|line| {
-            let row_str = line[0..7].replace("F", "0").replace("B", "1");
-            let col_str = line[7..].replace("L", "0").replace("R", "1");
-
-            let row = i32::from_str_radix(&row_str, 2).unwrap();
-            let col = i32::from_str_radix(&col_str, 2).unwrap();
-
-            (id(row, col), (row, col))
+            let id = line
+                .chars()
+                .fold(0, |acc, c| acc << 1 | ((c == 'B' || c == 'R') as i32));
+            id
         })
         .collect();
 
-    for row in 0..=127 {
-        for col in 0..=7 {
-            if !seats.contains_key(&id(row, col))
-                && seats.contains_key(&(id(row, col) + 1))
-                && seats.contains_key(&(id(row, col) - 1))
-            {
-                println!("Part 2: {}", id(row, col));
-                return;
-            }
+    for id in 0.. {
+        if !seats.contains(&id) && seats.contains(&(id + 1)) && seats.contains(&(id - 1)) {
+            println!("Part 2: {}", id);
+            return;
         }
     }
 }
+
 fn main() {
     p1();
     p2();
